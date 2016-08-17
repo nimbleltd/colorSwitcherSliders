43af9585b3d7be21e74ea9add5b2e071a7478f12
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-17 13:41:48 -0500

    simplified variable creation for r,g,b, this closes #1

--- colorSwitcherWithSliders/ViewController.swift
+++ colorSwitcherWithSliders/ViewController.swift
@@ -9,15 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Set colorView on launch
+        changeColorComponent()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeColorComponent() {
+        let currentRedValue = self.redControl.value
+        let r: CGFloat = CGFloat(currentRedValue)
+        let currentGreenValue = self.greenControl.value
+        let g: CGFloat = CGFloat(currentGreenValue)
+        let currentBlueValue = self.blueControl.value
+        let b: CGFloat = CGFloat(currentBlueValue)
+        
+//        print rgb values
+//        print("r = \(currentRedValue)")
+//        print("g = \(currentGreenValue)")
+//        print("b = \(currentBlueValue)")
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
